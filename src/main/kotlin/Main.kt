d532f794c0670fd8c9cd53c9e1c76e978980d0b9
Thiago Macedo <tl.macedo@hotmail.com>
2023-11-23 20:42:47 -0400

    03.01-lendo-id-do-jogo-buscado.

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -3,11 +3,20 @@ import java.net.URI
 import java.net.http.HttpClient
 import java.net.http.HttpRequest
 import java.net.http.HttpResponse
+import java.util.Scanner
 
 fun main() {
+
+    val leitura = Scanner(System.`in`)
+    print("Digite um código de jogo para buscar: ")
+
+    val busca = leitura.nextLine()
+
+    val urlGame = "https://www.cheapshark.com/api/1.0/games?id=$busca"
+
     val client = HttpClient.newHttpClient()
     val request = HttpRequest.newBuilder()
-        .uri(URI.create("https://www.cheapshark.com/api/1.0/games?id=146"))
+        .uri(URI.create(urlGame))
         .build()
     val response = client.send(request, HttpResponse.BodyHandlers.ofString())
 
