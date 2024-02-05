8bf0326c86d08a8eb3f2e4ff6e722405d1cc3cb8
tlmacedo <tl.macedo@hotmail.com>
2024-02-05 14:46:17 -0400

    01.06-refatorando-código

--- src/test/kotlin/TestJson.kt
+++ src/test/kotlin/TestJson.kt
@@ -1,8 +1,9 @@
 import br.com.alura.alugames.servicos.ConsumoApi
 
-fun main(){
+fun main() {
     val consumo = ConsumoApi()
     val listaGamers = consumo.buscarGamers()
-
+    val jogoApi = consumo.buscarJogo("151")
     println(listaGamers)
+    println(jogoApi)
 }
