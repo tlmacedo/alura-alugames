ac86e27a6361490cba3f999fd2c59e7553ba1834
tlmacedo <tl.macedo@hotmail.com>
2024-03-04 20:54:30 -0400

    03.03-implementando-metodos-genericos

--- src/main/kotlin/br/com/alura/alugames/dados/DAO.kt
+++ src/main/kotlin/br/com/alura/alugames/dados/DAO.kt
@@ -2,10 +2,15 @@ package br.com.alura.alugames.dados
 
 import javax.persistence.EntityManager
 
-abstract class DAO<TModel>(protected val manager: EntityManager) {
+abstract class DAO<TModel, TEntity>(protected val manager: EntityManager, protected val entityType: Class<TEntity>) {
 
-    abstract fun toEntity(objeto: TModel)
-    abstract fun getLista(): List<TModel>
+    abstract fun toEntity(objeto: TModel): TEntity
+    abstract fun toModel(entity: TEntity): TModel
+    open fun getLista(): List<TModel> {
+        val query = manager.createQuery("from ${entityType.simpleName} ", entityType)
+        return query.resultList.map { entity -> toModel(entity) }
+    }
+
     open fun adicionar(objeto: TModel) {
         val entity = toEntity(objeto)
         manager.transaction.begin()
--- src/main/kotlin/br/com/alura/alugames/dados/JogosDAO.kt
+++ src/main/kotlin/br/com/alura/alugames/dados/JogosDAO.kt
@@ -3,23 +3,25 @@ package br.com.alura.alugames.dados
 import br.com.alura.alugames.modelo.Jogo
 import javax.persistence.EntityManager
 
-class JogosDAO(manager: EntityManager) : DAO<Jogo>(manager) {
-    override fun toEntity(objeto: Jogo) {
-        return JogoEntity(objeto.titulo, objeto.capa, objeto.preco, objeto.descricao, objeto.id)
+class JogosDAO(manager: EntityManager) : DAO<Jogo, JogoEntity>(manager, JogoEntity::class.java) {
+    override fun toEntity(objeto: Jogo): JogoEntity {
+        return JogoEntity(
+            id = objeto.id,
+            titulo = objeto.titulo,
+            capa = objeto.capa,
+            preco = objeto.preco,
+            descricao = objeto.descricao
+        )
     }
 
-    override fun getLista(): List<Jogo> {
-        val query = manager.createQuery("from JogoEntity ", JogoEntity::class.java)
-        return query.resultList.map { entity ->
-            Jogo(
-                titulo = entity.titulo,
-                capa = entity.capa,
-                preco = entity.preco,
-                descricao = entity.descricao.toString(),
-                id = entity.id
-            )
-        }
+    override fun toModel(entity: JogoEntity): Jogo {
+        return Jogo(
+            id = entity.id,
+            titulo = entity.titulo,
+            capa = entity.capa,
+            preco = entity.preco,
+            descricao = entity.descricao
+        )
     }
-
 
 }
