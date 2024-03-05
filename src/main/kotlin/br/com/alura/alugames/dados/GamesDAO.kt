391afc816509d91dd516368815424814faa35fd9
tlmacedo <tl.macedo@hotmail.com>
2024-03-04 20:34:50 -0400

    03.02-utilizando-generics

--- src/main/kotlin/br/com/alura/alugames/dados/GamesDAO.kt
+++ src/main/kotlin/br/com/alura/alugames/dados/GamesDAO.kt
@@ -3,16 +3,16 @@ package br.com.alura.alugames.dados
 import br.com.alura.alugames.modelo.Gamer
 import javax.persistence.EntityManager
 
-class GamesDAO(private val manager: EntityManager) {
+class GamesDAO(private val manager: EntityManager) : DAO<Gamer>() {
 
-    fun getGamers(): List<Gamer> {
+    override fun getLista(): List<Gamer> {
         val query = manager.createQuery("from GamerEntity", GamerEntity::class.java)
         return query.resultList.map { entity ->
             Gamer(entity.nome, entity.email, entity.dataNascimento, entity.usuario.toString(), entity.id)
         }
     }
 
-    fun adicionaGamer(gamer: Gamer) {
+    override fun adicionar(gamer: Gamer) {
         val entity = GamerEntity(gamer.id, gamer.nome, gamer.email, gamer.dataNascimento, gamer.usuario)
         manager.transaction.begin()
         manager.persist(entity)
--- src/main/kotlin/br/com/alura/alugames/dados/JogosDAO.kt
+++ src/main/kotlin/br/com/alura/alugames/dados/JogosDAO.kt
@@ -3,9 +3,9 @@ package br.com.alura.alugames.dados
 import br.com.alura.alugames.modelo.Jogo
 import javax.persistence.EntityManager
 
-class JogosDAO(val manager: EntityManager) {
+class JogosDAO(val manager: EntityManager) : DAO<Jogo>() {
 
-    fun getJogos(): List<Jogo> {
+    override fun getLista(): List<Jogo> {
         val query = manager.createQuery("from JogoEntity ", JogoEntity::class.java)
         return query.resultList.map { entity ->
             Jogo(
@@ -18,7 +18,7 @@ class JogosDAO(val manager: EntityManager) {
         }
     }
 
-    fun adicionarJogo(jogo: Jogo) {
+    override fun adicionar(jogo: Jogo) {
         val entity = JogoEntity(
             titulo = jogo.titulo,
             capa = jogo.capa,
